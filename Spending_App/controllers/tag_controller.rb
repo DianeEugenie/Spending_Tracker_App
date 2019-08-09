21542fa72571647cf6160f4ca05a8bbd866d1f98
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-09 21:07:14 +0100

    Creates New and Create route for Tags

--- Spending_App/controllers/tag_controller.rb
+++ Spending_App/controllers/tag_controller.rb
@@ -7,17 +7,24 @@ get '/tags' do
   erb(:'tags/index')
 end
 
+#NEW
+get '/tags/new' do
+  erb(:'tags/new')
+end
+
+#CREATE
+
+post '/tags' do
+  tag = Tag.new(params)
+  tag.save()
+  redirect '/tags'
+end
+
 #SHOW
 
 get '/tags/:id' do
   id = params[:id].to_i
   @tag = Tag.find(id)
+  @merchants = Merchant.all()
   erb(:'tags/show')
-end
-
-
-
-#NEW
-get '/tags/new' do
-  erb(:'tags/new')
 end
