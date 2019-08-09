bef853b4becbedb96c622d8c418e225d7cde1d02
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-09 20:51:46 +0100

    Creates Show Route for Tags

--- Spending_App/controller.rb
+++ Spending_App/controller.rb
@@ -2,6 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all')
 also_reload('../models/*' )
 require_relative('./controllers/tag_controller.rb')
+
+require('pry-byebug')
 # require_relative('../controllers/merchant_controller.rb')
 # require_relative('../controllers/transaction_controller.rb')
 
--- Spending_App/controllers/tag_controller.rb
+++ Spending_App/controllers/tag_controller.rb
@@ -1,6 +1,23 @@
 require_relative('../models/tag.rb')
+require_relative('../models/merchant.rb')
 
+#INDEX
 get '/tags' do
   @tags = Tag.all()
   erb(:'tags/index')
+end
+
+#SHOW
+
+get '/tags/:id' do
+  id = params[:id].to_i
+  @tag = Tag.find(id)
+  erb(:'tags/show')
+end
+
+
+
+#NEW
+get '/tags/new' do
+  erb(:'tags/new')
 end
