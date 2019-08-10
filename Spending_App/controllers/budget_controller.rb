20bdf9d55d0ba3638d0036332c6c92caa50f179e
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-10 19:41:13 +0100

    Create New and Ediy Route for Budget

--- Spending_App/controllers/budget_controller.rb
+++ Spending_App/controllers/budget_controller.rb
@@ -8,6 +8,18 @@ get '/budgets' do
   erb(:'budgets/index')
 end
 
+#NEW
+get '/budgets/new' do
+  erb(:'/budgets/new')
+end
+
+#CREATE
+post '/budgets' do
+  budget = Budget.new(params)
+  budget.save()
+  redirect '/budgets'
+end
+
 #SHOW
 
 get '/budgets/:id' do
